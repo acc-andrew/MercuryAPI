5b86ee8376d0c02a3241ad65bf4210a28b5c3133
acc-andrew <46398412+acc-andrew@users.noreply.github.com>
2025-03-21 14:38:17 +0800

    main branch

---
+++ MercuryAPI/Controllers/MyofficeACPD.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MercuryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyofficeACPD : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return "Hello World!";
+        }
+    }
+
+}
